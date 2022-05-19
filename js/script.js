c7acb041a6223ee86030349cd0fe6bb52b961daa
Gloria <gloghe@yahoo.it>
2022-05-19 15:28:59 +0200

    struttura

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ iconJs.forEach((element) => {
     const creatoDivCard = document.createElement("div");
     creatoDivCard.classList.add("card");
     const creatoDivImg = document.createElement("i");
-    creatoDivImg.classList.add(element.family,  element.prefix + element.name);
+    creatoDivImg.classList.add(element.family,  element.prefix + element.name, "iconSize");
     creatoDivCard.append(creatoDivImg);
     const creatoDivText = document.createElement("span");
     creatoDivText.classList.add("iconText");
-    creatoDivText.innerHTML = element.name;
+    creatoDivText.innerHTML = (element.name).toUpperCase();
     creatoDivCard.append(creatoDivText);
     stampa.append(creatoDivCard);
 
